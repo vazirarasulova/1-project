5527b51edabc99c0562ffb2183c83c8d61ae1bd2
vazirarasulova <rasulovavazira202gmail.com>
2022-02-21 13:37:52 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+var moneyQuantity = Number(prompt("Pul miqdorini so'mda kiriting"));
+
+var usd = 750;
+var euro = 120;
+
+var convertDollor = usd * 10864.96;
+var convertEuro = euro * 12359.65;
+var amountMoney = convertDollor + convertEuro;
+
+console.log("amountMoney = "  + amountMoney);
+
+if (moneyQuantity >= amountMoney) {
+    console.log("Oq yo'l, Alisher!");
+}else {
+    console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
+} 
+
