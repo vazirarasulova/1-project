a1147ce43cdae5efadfcbd49a5ccef97cbd8f8ea
vazirarasulova <rasulovavazira202gmail.com>
2022-06-08 00:03:46 +0500

    Correct the mistake

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 var moneyQuantity = Number(prompt("Pul miqdorini so'mda kiriting"));
 
+
+
 var usd = 750;
 var euro = 120;
 
@@ -8,11 +10,14 @@ var convertDollor = usd * 10864.96;
 var convertEuro = euro * 12359.65;
 var amountMoney = convertDollor + convertEuro;
 
-console.log("amountMoney = "  + amountMoney);
+
 
 if (moneyQuantity >= amountMoney) {
-    console.log("Oq yo'l, Alisher!");
+console.log("Oq yo'l, Alisher!");
+
+
 }else {
     console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.");
-} 
+}
+
 
